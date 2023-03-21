931a08b5128aeb0d3d37111332d521f7291c0793
정호균 <saegyun0915@konkuk.ac.kr>
2023-03-21 20:35:25 +0900

    success!

--- 1주차스터디/2065-나룻배/main.cpp
+++ 1주차스터디/2065-나룻배/main.cpp
@@ -18,13 +18,15 @@ int n, m, t, current = 0, amount = 0;
 bool isStart = 1;
 bool isLeft = 1;
 
-vector<pair<int, string>> input_timeline;
-queue<pair<int, string>> boarding_people;
+vector<pair<pair<int, int>, string>> input_timeline;
+vector<pair<pair<int, int>, string>> tmp_timeline;
+
+queue<pair<pair<int, int>, string>> boarding_people;
 
 map<string, int> answer;
 
-queue<int> left_q;
-queue<int> right_q;
+queue<pair<int, int>> left_q;
+queue<pair<int, int>> right_q;
 
 int main() {
     fastio;
@@ -34,12 +36,13 @@ int main() {
         int arrival;
         string pos;
         cin >> arrival >> pos;
-        input_timeline.push_back({ arrival, pos });
+        input_timeline.push_back({ { arrival, i }, pos });
     }
 
-    sort(allV(input_timeline));
+    tmp_timeline = input_timeline;
+    sort(allV(tmp_timeline));
 
-    for (pair<int, string> p : input_timeline) {
+    for (pair<pair<int, int>, string> p : tmp_timeline) {
         if (p.ss == "left") {
             left_q.push(p.ff);
         }
@@ -53,15 +56,18 @@ int main() {
     while (1) {
         if (!isStart) {
             current += t;
-            //cout << "ferry moved at " << current << "\n";
 
             isLeft = !isLeft;
+            //cout << "ferry moved at " << current << " to " << (isLeft ? "Left" : "Right") << "\n";
+            
             amount = 0;
             while (!boarding_people.empty()) {
-                pair<int, string> person = boarding_people.front();
+                pair<pair<int, int>, string> person = boarding_people.front();
+                //cout << person.ff << " from " << person.ss << " leave at " << current << "\n";
                 boarding_people.pop();
 
-                string key = to_string(person.ff) + "_" + person.ss;
+                //cout << current << "\n";
+                string key = to_string(person.ff.ff) + "_" + person.ss + "_" + to_string(person.ff.ss);
 
                 answer[key] = current;
             }
@@ -78,13 +84,17 @@ int main() {
             //cout << "status - boarding_count : " << boarding_people.size() << "\tis left? : " << isLeft << "\n";
             // 앞으로 올 사람이 없으면 넘기기
             if (left_q.empty()) {
-                continue;
+                if (!right_q.empty()) {
+                    current = max(right_q.front().ff, current);
+                    continue;
+                }
+                break;
             }
             // 현재 시간에 탈 사람이 있을 경우
             while (!left_q.empty()) {
-                int arrival_time = left_q.front();
+                pair<int, int> arrival_time = left_q.front();
 
-                if (arrival_time <= current) {
+                if (arrival_time.ff <= current) {
                     //cout << "left : " << arrival_time << " is on board\n";
                     boarding_people.push({ arrival_time, "left" });
                     left_q.pop();
@@ -96,20 +106,24 @@ int main() {
                     break;
                 }
             }
+
             // 탈 사람이 없을 경우
             if (boarding_people.size() == 0) {
-                if (right_q.front() < left_q.front()) {
-                    // 만약 오른쪽에 사람이 먼저 오면 일단 이동
-                    current = right_q.front();
-                    continue;
+                if (!right_q.empty()) {
+                    if (right_q.front() < left_q.front()) {
+                        // 만약 오른쪽에 사람이 먼저 오면 일단 이동
+                        current = max(right_q.front().ff, current);
+                        continue;
+                    }
                 }
                 // 왼쪽에 사람이 먼저 왔으면 그 시간에 온 모든 사람을 최대 수 까지 태움
-                current = left_q.front();
+                current = left_q.front().ff;
 
                 while (!left_q.empty()) {
-                    int arrival_time = left_q.front();
+                    pair<int, int> arrival_time = left_q.front();
+                    //cout << "left : " << arrival_time << " is on board\n";
 
-                    if (arrival_time <= current) {
+                    if (arrival_time.ff <= current) {
                         boarding_people.push({ arrival_time, "left" });
                         left_q.pop();
                     }
@@ -126,13 +140,18 @@ int main() {
             //cout << "status - boarding_count : " << boarding_people.size() << "\tis left? : " << isLeft << "\n";
             // 앞으로 올 사람이 없으면 넘기기
             if (right_q.empty()) {
-                continue;
+
+                if (!left_q.empty()) {
+                    current = max(left_q.front().ff, current);
+                    continue;
+                }
+                break;
             }
             // 현재 시간에 탈 사람이 있을 경우
             while (!right_q.empty()) {
-                int arrival_time = right_q.front();
+                pair<int, int> arrival_time = right_q.front();
 
-                if (arrival_time <= current) {
+                if (arrival_time.ff <= current) {
                     //cout << "right : " << arrival_time << " is on board\n";
                     boarding_people.push({ arrival_time, "right" });
                     right_q.pop();
@@ -146,18 +165,22 @@ int main() {
             }
             // 탈 사람이 없을 경우
             if (boarding_people.size() == 0) {
-                if (right_q.front() > left_q.front()) {
-                    // 만약 왼쪽에 사람이 먼저 오면 일단 이동
-                    current = left_q.front();
-                    continue;
+                if (!left_q.empty()) {
+                    if (right_q.front() > left_q.front()) {
+                        // 만약 왼쪽에 사람이 먼저 오면 일단 이동
+                        current = max(left_q.front().ff, current);
+                        continue;
+                    }
+
                 }
                 // 오른쪽에 사람이 먼저 왔으면 그 시간에 온 모든 사람을 최대 수 까지 태움
-                current = right_q.front();
+                current = right_q.front().ff;
 
                 while (!right_q.empty()) {
-                    int arrival_time = right_q.front();
+                    pair<int, int> arrival_time = right_q.front();
+                    //cout << "right : " << arrival_time << " is on board\n";
 
-                    if (arrival_time <= current) {
+                    if (arrival_time.ff <= current) {
                         boarding_people.push({ arrival_time, "right" });
                         right_q.pop();
                     }
@@ -171,8 +194,8 @@ int main() {
             }
         }
     }
-    for (pair<int, string> person : input_timeline) {
-        cout << answer[to_string(person.ff) + "_" + person.ss] << "\n";
+    for (pair<pair<int, int>, string> person : input_timeline) {
+        cout << answer[to_string(person.ff.ff) + "_" + person.ss + "_" + to_string(person.ff.ss)] << "\n";
     }
 
     return 0;
